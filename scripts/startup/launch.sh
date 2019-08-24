07180902a06bc0246ecf3454397f60cc7c7ff8bc
Jeppefs <s.jeppe.f@gmail.com>
2019-08-24 17:11:04 +0000

    added to launch script

--- scripts/startup/launch.sh
+++ scripts/startup/launch.sh
@@ -1,9 +1,23 @@
+i3-msg 'workspace 1; exec kitty' 
+sleep 1 
+
+i3-msg 'workspace 2; exec firefox'
+sleep 1
+
+i3-msg workspace 5
+code /$HOME/myconfig
+kitty 
+echo "cd /$HOMR/myconfig"
+
+i3-msg 'workspace 8; exec spotify'
+i3-msg 'workspace 8; exec discord'
+
 i3-msg workspace Welcome 
 sleep 1
 code /$HOME/Dropbox/Lists
 sleep 1
 i3-msg 'workspace Welcome; exec kitty' 
-#kitty --hold --detach 
 sleep 1
 i3-msg 'split v'
-i3-msg 'kitty --hold --detach neofetch'
+i3-msg 'workspace Welcome; exec "kitty --hold --detach neofetch"'
+
