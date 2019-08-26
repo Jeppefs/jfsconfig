8cfa6776e934828ff2e64c0919eb430f14b67241
Jeppefs <s.jeppe.f@gmail.com>
2019-08-26 20:29:34 +0000

    upgraded launch.sh

--- scripts/startup/launch.sh
+++ scripts/startup/launch.sh
@@ -4,13 +4,9 @@ sleep 1
 i3-msg 'workspace 2; exec firefox'
 sleep 1
 
-i3-msg workspace 5
-code $HOME/myconfig
-kitty --hold
-echo "cd $HOME/myconfig"
-
-i3-msg 'workspace 8; exec spotify'
-i3-msg 'workspace 8; exec discord'
+i3-msg 'workspace 5; exec code $HOME/myconfig'
+sleep 1
+i3-msg "workspace 5; exec kitty -d $HOME/myconfig"
 
 i3-msg workspace Welcome 
 sleep 1
@@ -20,4 +16,8 @@ i3-msg 'workspace Welcome; exec kitty'
 sleep 1
 i3-msg 'split v'
 i3-msg 'workspace Welcome; exec "kitty --hold --detach neofetch"'
+sleep 5 
 
+i3-msg 'workspace 8; exec spotify'
+sleep 1
+i3-msg 'workspace 8; exec discord'
