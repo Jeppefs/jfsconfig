8993a78c9977261825fc67ae7290d092a54c4caa
Jeppefs <s.jeppe.f@gmail.com>
2019-09-10 15:33:34 +0000

    Got nerd fonts to work in i3

--- scripts/startup/launch.sh
+++ scripts/startup/launch.sh
@@ -5,23 +5,23 @@ i3-msg 'workspace 2; exec firefox'
 sleep 1
 
 i3-msg 'workspace 5; exec code $HOME/myconfig'
-sleep 1
+sleep 2
 i3-msg 'workspace 5; exec kitty -d $HOME/myconfig'
 sleep 1
 
 i3-msg workspace Welcome 
-sleep 5
-i3-msg 'workspace Welcome; exec code $HOME/Dropbox/Lists'
 sleep 1
+i3-msg 'workspace Welcome; exec code $HOME/Dropbox/Lists'
+sleep 2
 i3-msg 'workspace Welcome; exec kitty' 
 sleep 1
 i3-msg 'split v'
 i3-msg 'workspace Welcome; exec "kitty --hold --detach neofetch"'
-sleep 5 
+sleep 1 
 
 i3-msg 'workspace 8; exec spotify'
 sleep 1
 i3-msg 'workspace 8; exec discord'
-sleep 1
+sleep 5
 
 i3-msg workspace Welcome 
