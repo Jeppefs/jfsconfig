0ccabfeecd4134f451e9740c9e1bbb9407502a11
Jeppefs <s.jeppe.f@gmail.com>
2019-09-10 17:59:28 +0000

    made polybar

--- scripts/startup/launch.sh
+++ scripts/startup/launch.sh
@@ -1,5 +1,19 @@
 dmenu_extended_cache_build
 
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar example &
+
+echo "Polybar launched..."
+
+## Launch Programs
 i3-msg 'workspace 1; exec kitty' 
 sleep 1 
 
