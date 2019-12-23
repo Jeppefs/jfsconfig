562f196c2ac27f669b1133892a4748e41110b4d9
Jeppefs <s.jeppe.f@gmail.com>
2019-12-23 14:02:27 +0100

    ready for merge

--- scripts/startup/launch.sh
+++ scripts/startup/launch.sh
@@ -23,14 +23,14 @@ i3-msg 'workspace 2; exec firefox'
 sleep 1
 
 i3-msg 'workspace 5; exec code $HOME/myconfig'
-sleep 2
+sleep 5
 i3-msg 'workspace 5; exec kitty -d $HOME/myconfig'
 sleep 1
 
 i3-msg workspace Welcome 
 sleep 1
 i3-msg 'workspace Welcome; exec code $HOME/Dropbox/Lists'
-sleep 2
+sleep 5
 i3-msg 'workspace Welcome; exec kitty' 
 sleep 1
 i3-msg 'split v'
