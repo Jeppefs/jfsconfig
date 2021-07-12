d69bc8a1b41746b1584845c1602863bd15f110c6
Jeppefs <s.jeppe.f@gmail.com>
2021-07-12 12:24:28 +0200

    changed startup script

--- scripts/startup/launch.sh
+++ scripts/startup/launch.sh
@@ -20,28 +20,11 @@ sleep 1
 i3-msg 'move scratchpad'
 sleep 1
 
-# TODO: Make secondary firefox move to workspace 9
 i3-msg 'workspace 1; exec firefox; gaps current inner set 0'
-sleep 2
-# i3-msg 'focus left'
-# sleep 1
-i3-msg 'move container to workspace 9'
-sleep 1
-
-i3-msg 'workspace 5; exec code $HOME/myconfig'
-sleep 5
-i3-msg 'workspace 5; exec kitty -d $HOME/myconfig'
-sleep 1
-
-i3-msg workspace Q
-sleep 5
-i3-msg 'workspace Welcome; exec kitty' 
-sleep 1
-i3-msg 'split v'
-i3-msg 'workspace Welcome; exec "kitty --hold --detach neofetch"'
-sleep 1 
+sleep 3
 
 i3-msg 'workspace 10; exec discord'
-sleep 5
+sleep 8
 
-i3-msg workspace Q
+i3-msg 'workspace Q'
+i3-msg 'exec emacs'
